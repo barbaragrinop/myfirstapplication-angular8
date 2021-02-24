195a146724d26d0e472691732a52efe2456db035
Barbara Hellen <barbarapereira123@hotmail.com>
2021-02-24 11:32:34 -0300

    Minha primeira aplicação em Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MyFirstComponent} from './myfirst.component'
 import { AppComponent } from './app.component';
+import { MySecondComponent } from './my-second/my-second.component';
+import { MyThirdComponent } from './my-second/my-third/my-third.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponent,
+    MySecondComponent,
+    MyThirdComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/myfirst.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'myfirst', //tag
+    templateUrl: './myfirst.component.html', //qual html vai carregar
+    styleUrls: ['./myfirst.component.css'] 
+})
+
+export class MyFirstComponent{
+    
+}
